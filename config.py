03942c67183783784a9affa8b9081186a876e2cd
Thiago Barros De Jaegher <thiagobdjaegher@yahoo.com.br>
2020-05-13 09:13:08 -0300

    Securite for sensitive data

--- config.py
+++ config.py
@@ -7,13 +7,12 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
 
-    FLASK_ENV = os.environ.get('FLASK_ENV')
-    SECRET_KEY = os.environ.get('KEY')
+    SECRET_KEY = os.environ.get('S_KEY')
 
     # Database
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DF_DB_URI')
     SQLALCHEMY_BINDS = {
-        'sgh': os.environ.get('SGH_DATABASE_URI')
+        'sgh': os.environ.get('SGH_DB_URI')
     }
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
